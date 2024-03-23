56f58269aae8e75b00ff16d6c4f6206fc3169471
koverchik <koverchik.o@gmail.com>
2024-03-23 14:56:49 +0300

    change color for black theme and fix date

--- src/Components/Experience.jsx
+++ src/Components/Experience.jsx
@@ -4,19 +4,19 @@ export const  Experience = ()=> {
             <div className={'custom-list'}>
                 <div className={'basic-information-custom-list'}>
                     <p className={'title-basic-information-custom-list'}>Amasty</p>
-                    <div><p>September 2024</p><span>—</span><p>till now</p></div>
+                    <div><p>September 2023</p><span>—</span><p>till now</p></div>
                 </div>
                 <div className={'description-custom-list'}>
                     <p className={'title-description-custom-list'}>{`${('Software developer').toUpperCase()}`}</p>
-                    <p className={'content-description-custom-list'}>
-                        Development of a microservices architecture to enhance the functionality and scalability of the system, utilizing the Symphony framework for service construction.
-                        Integration of microservices using RabbitMQ to facilitate asynchronous communication between system components.
-                        Creation and optimization of databases using PostgreSQL.
-                        Development of APIs for interaction between microservices and external systems.
-                        Testing and debugging microservices.
-                        Creation of automation deployment for microservices using Docker and Git.
-                        Documentation of development processes in Confluence, utilizing Jira.
-                    </p>
+                    <div className={'content-description-custom-list'}>
+                        <p>Development of a microservices architecture to enhance the functionality and scalability of the system, utilizing the Symphony framework for service construction.</p>
+                        <p>Integration of microservices using RabbitMQ to facilitate asynchronous communication between system components.</p>
+                        <p>Creation and optimization of databases using PostgreSQL.</p>
+                        <p>Development of APIs for interaction between microservices and external systems.</p>
+                        <p>Testing and debugging microservices.</p>
+                        <p>Creation of automation deployment for microservices using Docker and Git.</p>
+                        <p>Documentation of development processes in Confluence, utilizing Jira.</p>
+                    </div>
                 </div>
             </div>
             <div className={'custom-list'}>
@@ -28,16 +28,15 @@ export const  Experience = ()=> {
                 </div>
                 <div className={'description-custom-list'}>
                     <p className={'title-description-custom-list'}>{`${('PHP-programmer').toUpperCase()}`}</p>
-                    <p className={'content-description-custom-list'}>
-                        Development and maintenance of web applications using programming languages and frameworks:
-                        React, PHP and Python.
-                        Participation in all stages of development, including requirements analysis, design, development, testing, and deployment.
-                        Creation and optimization of databases for web applications.
-                        Integration of web applications with external services and APIs.
-                        Collaboration within a development team and coordination with other departments: designers, QA engineers.
-                        Utilization of Git version control systems for code management.
-                        Documentation of development processes in Confluence. Using Jira and Teams for corporate and private work arrangement.
-                    </p>
+                    <div className={'content-description-custom-list'}>
+                        <p>Development and maintenance of web applications using programming languages and frameworks: React, PHP and Python.</p>
+                        <p>Participation in all stages of development, including requirements analysis, design, development, testing, and deployment.</p>
+                        <p>Creation and optimization of databases for web applications.</p>
+                        <p>Integration of web applications with external services and APIs.</p>
+                        <p>Collaboration within a development team and coordination with other departments: designers, QA engineers.</p>
+                        <p>Utilization of Git version control systems for code management.</p>
+                        <p>Documentation of development processes in Confluence. Using Jira and Teams for corporate and private work arrangement.</p>
+                    </div>
                 </div>
             </div>
             <div className={'custom-list'}>
@@ -47,12 +46,12 @@ export const  Experience = ()=> {
                 </div>
                 <div className={'description-custom-list'}>
                     <p className={'title-description-custom-list'}>{`${('Programmer').toUpperCase()}`}</p>
-                    <p className={'content-description-custom-list'}>
-                        Development of mobile applications using React Native.
-                        Integration of mobile applications with web services and APIs, ensuring efficient interaction with the server-side.
-                        Optimization and testing mobile applications.
-                        Participation in the implementation of new features and updates in mobile applications.
-                    </p>
+                    <div className={'content-description-custom-list'}>
+                        <p>Development of mobile applications using React Native.</p>
+                        <p>Integration of mobile applications with web services and APIs, ensuring efficient interaction with the server-side.</p>
+                        <p>Optimization and testing mobile applications.</p>
+                        <p>Participation in the implementation of new features and updates in mobile applications.</p>
+                    </div>
                 </div>
             </div>
             <div className={'custom-list'}>
